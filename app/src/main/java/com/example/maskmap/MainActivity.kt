f2477adde59c062302e5ab3e11ebc6a48e30aa57
alex <a4704704@gmail.com>
2021-08-07 18:40:33 +0800

    加入網路連線

--- app/src/main/java/com/example/maskmap/MainActivity.kt
+++ app/src/main/java/com/example/maskmap/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.maskmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getPharmacyData()
     }
+}
+
+private fun getPharmacyData() {
+    //口罩資料網址
+    val pharmaciesDataUrl = "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
+
+    //Part 1: 宣告 OkHttpClient
+    val okHttpClient = OkHttpClient().newBuilder().build()
+
+    //Part 2: 宣告 Request，要求要連到指定網址
+    val request: Request = Request.Builder().url(pharmaciesDataUrl).get().build()
+
+    //Part 3: 宣告 Call
+    val call = okHttpClient.newCall(request)
+
+    //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
+    call.enqueue(object : Callback {
+        override fun onFailure(call: Call, e: java.io.IOException) {
+            Log.d("HKT", "onFailure: $e")
+        }
+
+        override fun onResponse(call: Call, response: Response) {
+            Log.d("HKT", "onResponse: ${response.body?.string()}")
+        }
+    })
 }
