c5e0200a4cfba3e6bd33cd5de864859ee1bd0ca9
Cong Hua <a4704704@gmail.com>
2021-09-07 16:10:16 +0800

    進階項目佈局 Layout 實作

--- app/src/main/java/com/example/maskmap/MainActivity.kt
+++ app/src/main/java/com/example/maskmap/MainActivity.kt
@@ -56,12 +56,12 @@ class MainActivity : AppCompatActivity() {
             setHasFixedSize(true)
 
             //每個項目，加入分隔線(divider)，讓每筆資料看起來更清楚
-            addItemDecoration(
-                DividerItemDecoration(
-                    this@MainActivity,
-                    DividerItemDecoration.VERTICAL
-                )
-            )
+//            addItemDecoration(
+//                DividerItemDecoration(
+//                    this@MainActivity,
+//                    DividerItemDecoration.VERTICAL
+//                )
+//            )
         }
     }
 
@@ -104,72 +104,4 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-//    封裝OkHtttp前
-//    private fun getPharmacyData() {
-//        binding.progressBar.visibility = View.VISIBLE
-//
-//        //口罩資料網址
-//        val pharmaciesDataUrl = "https://raw.githubusercontent.com/thishkt/pharmacies/master/data/info.json"
-//
-//        //Part 1: 宣告 OkHttpClient，.build()代表建置這個方法
-//        val okHttpClient = OkHttpClient().newBuilder().build()
-//
-//        //Part 2: 宣告 Request，要求要連到指定網址
-//        val request = Request.Builder().url(pharmaciesDataUrl).get().build()
-//
-//        //Part 3: 宣告 Call，newCall(填入要求)
-//        val call = okHttpClient.newCall(request)
-//
-//        //執行 Call 連線後，採用 enqueue 非同步方式，獲取到回應的結果資料
-//        call.enqueue(object : Callback {
-//            override fun onFailure(call: Call, e: java.io.IOException) {
-//                Log.d("HKT", "onFailure: $e")
-//
-//                //注意要設定 UI ，需要執行在 UiThread 裡面，否則會噴錯誤
-//                runOnUiThread {
-//                    binding.progressBar.visibility = View.GONE
-//                }
-//            }
-//            override fun onResponse(call: Call, response: Response) {
-//
-//                var propertiesName = StringBuilder()
-//
-//                //從 Okhttp 收到的回應資料 response，取出 body 的部分。
-//                //注意這裏，response 不能二次使用，不然會噴錯誤。
-//                //所以我們將他轉存到 pharmaciesData 。
-//                val pharmaciesData = response.body?.string()
-//
-//                val pharmacyInfo = Gson().fromJson(pharmaciesData, PharmacyInfo::class.java)
-//
-//                for(i in pharmacyInfo.features) {
-//                    propertiesName.append(i.properties.name + "\n")
-//                }
-//
-//                //將 pharmaciesData 整包字串資料，轉成 JSONObject 格式
-////                val obj = JSONObject(pharmaciesData)
-//
-//                //這個時候，我們就可以透過 getString 的方式，裡面放 key (name) 值，
-//                //即可以獲取到最外層的 type 欄位資料值。
-//                //Log.d("HKT",obj.getString("type"))
-//
-//                //features 是一個陣列 [] ，需要將他轉換成 JSONArray
-////                val featuresArray = JSONArray(obj.getString("features"))
-//
-////                val propertiesName = StringBuilder()
-//                //透過 for 迴圈，即可以取出所有的藥局名稱
-////                for (i in 0 until featuresArray.length()) {
-////                    val properties = featuresArray.getJSONObject(i).getString("properties")
-////                    val property = JSONObject(properties)
-////                    Log.d("HKT", "name: ${property.getString("name")}")
-////                    propertiesName.append(property.getString("name") + "\n")
-////                }
-//
-//                runOnUiThread{
-//                    //將 Okhttp 獲取到的回應值，指定到畫面的 TextView 元件中
-//                    binding.tvPharmaciesData.text = propertiesName
-//                    binding.progressBar.visibility = View.GONE
-//                }
-//            }
-//        })
-//    }
 }
--- app/src/main/java/com/example/maskmap/MainAdapter.kt
+++ app/src/main/java/com/example/maskmap/MainAdapter.kt
@@ -7,18 +7,14 @@ import com.example.maskmap.data.Feature
 import com.example.maskmap.databinding.ItemViewBinding
 
 //RecyclerView.Adapter<加入定義畫面的view，也就是MyViewHolder>()
-class MainAdapter : RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
+class MainAdapter(private val itemClickListener: IItemClickListener) :
+    RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
 
     var pharmacyList :List<Feature> = emptyList()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
-
-    //讓view來自item_view
-    //MyViewHolder必須繼承RecyclerView的ViewHolder
-    class MyViewHolder(val itemViewBinding: ItemViewBinding) :
-        RecyclerView.ViewHolder(itemViewBinding.root)
 
     //用於決定資料型態。
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -30,12 +26,29 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
     }
 
     //決定元件上要顯示的資料
+    //position即{0,1,2,3,...}
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemViewBinding.tvName.text = pharmacyList[position].properties.name
+
+        holder.itemViewBinding.tvAdultAmount.text = pharmacyList[position].properties.mask_adult.toString()
+        holder.itemViewBinding.tvChildAmount.text = pharmacyList[position].properties.mask_child.toString()
+
+        holder.itemViewBinding.layoutItem.setOnClickListener {
+            itemClickListener.onItemClickListener(pharmacyList[position])
+        }
     }
 
     //決定資料大小
     override fun getItemCount(): Int {
         return pharmacyList.size
+    }
+
+    //讓view來自item_view
+    //MyViewHolder必須繼承RecyclerView的ViewHolder
+    class MyViewHolder(val itemViewBinding: ItemViewBinding) :
+        RecyclerView.ViewHolder(itemViewBinding.root)
+
+    interface IItemClickListener {
+        fun onItemClickListener(data: Feature)
     }
 }
